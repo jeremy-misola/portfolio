68745075a3510a35d6dd032fa90784b05fc8e2d1
jeremy-misola <misolarellinj06@gmail.com>
2025-08-21 15:08:26 -0400

    restored checkpoint

--- backend/services/pipeline/routes.go
+++ backend/services/pipeline/routes.go
@@ -1,14 +1,14 @@
 package pipeline
 
 import (
-	"fmt"
+	"backend/utils"
+	"io"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-type Handler struct {
-}
+type Handler struct{}
 
 func NewHandler() *Handler {
 	return &Handler{}
@@ -16,14 +16,27 @@ func NewHandler() *Handler {
 
 func (h *Handler) RegisterRoutes(router *mux.Router) {
 	router.HandleFunc("/pipeline", h.handlePipeline).Methods("GET")
-
 }
 
 func (h *Handler) handlePipeline(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("test")
-	fmt.Fprintf(w, "test")
-	response, err := http.Get("http://api.github.com/repos/jeremy-misola/portfolio")
-	if err!=nil{
-		fm
+	repoStats, err := callGithubAPI("repos/jeremy-misola/portfolio")
+	if err != nil {
+		utils.WriteError(w, 503, err)
 	}
+	utils.WriteJSON(w, 200, string(repoStats))
+}
+
+func callGithubAPI(endpoint string) ([]byte, error) {
+	baseURL := "http://api.github.com/"
+	resp, err := http.Get(baseURL + endpoint)
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
 }
