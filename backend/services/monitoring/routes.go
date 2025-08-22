a0295716fd944c98f9b3e63a57ca9d67ffbda52b
jeremy-misola <misolarellinj06@gmail.com>
2025-08-21 20:40:16 -0400

    restore

--- backend/services/monitoring/routes.go
+++ backend/services/monitoring/routes.go
@@ -1,13 +1,13 @@
 package monitoring
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-type Handler struct {
-}
+type Handler struct{}
 
 func NewHandler() *Handler {
 	return &Handler{}
@@ -17,13 +17,17 @@ func (h *Handler) RegisterRoutes(router *mux.Router) {
 	router.HandleFunc("/monitoring/application-metrics", h.handleApplicationMetrics).Methods("GET")
 	router.HandleFunc("/monitoring/status", h.handleStatus).Methods("GET")
 	router.HandleFunc("/monitoring/infrastructure-metrics", h.handleInfrastructureMetrics).Methods("GET")
-
 }
-
 
 func (h *Handler) handleApplicationMetrics(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("testing")
 }
+
 func (h *Handler) handleStatus(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("status")
 }
+
 func (h *Handler) handleInfrastructureMetrics(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("infra")
 }
+
