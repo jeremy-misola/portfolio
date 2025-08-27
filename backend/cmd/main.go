ed23b8b688b5d57b6c65eb869474fe82e2f0babd
jeremy-misola <misolarellinj06@gmail.com>
2025-08-26 21:39:15 -0400

    add kubernetes client

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -3,15 +3,19 @@ package main
 import (
 	"backend/cmd/api"
 	"backend/config"
+	"backend/platform/kubernetes"
 	"log"
 )
 
 func main() {
-	// injecting config into apiServer
-	server := api.NewAPIServer(":8080", config.Envs)
-	err := server.Run()
+	kubeClient, err := kubernetes.CreateKubeClient()
 	if err != nil {
 		log.Fatal(err)
-
+	}
+	// injecting config into apiServer
+	server := api.NewAPIServer(":8080", config.Envs, kubeClient)
+	runErr := server.Run()
+	if runErr != nil {
+		log.Fatal(runErr)
 	}
 }
