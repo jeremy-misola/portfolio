9abd3f18832a96efc292f9dbbe823f9fe63cbf5c
Jeremy Misola <misolarellinj06@gmail.com>
2025-08-29 13:07:59 -0400

    test logging feature

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -4,10 +4,14 @@ import (
 	"backend/cmd/api"
 	"backend/config"
 	"backend/platform/kubernetes"
+	"fmt"
 	"log"
+	"os"
 )
 
 func main() {
+	fmt.Println("testing123")
+	os.Stdout.Sync()
 	kubeClient, err := kubernetes.CreateKubeClient()
 	if err != nil {
 		log.Fatal(err)
