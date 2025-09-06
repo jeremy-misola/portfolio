cf2197473dc32fa298ef68fc46816f28e094a0a4
Jeremy Misola <misolarellinj06@gmail.com>
2025-09-06 15:39:14 -0400

    change routes.go

--- backend/services/monitoring/routes.go
+++ backend/services/monitoring/routes.go
@@ -4,8 +4,11 @@ import (
 	"backend/utils"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
@@ -15,7 +18,26 @@ func NewHandler() *Handler {
 	return &Handler{}
 }
 
+func recordMetrics() {
+	fmt.Println("testing123")
+	go func() {
+		for {
+			opsProcessed.Inc()
+			time.Sleep(2 * time.Second)
+		}
+	}()
+}
+
+var (
+	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "myapp_processed_ops_total",
+		Help: "The total number of processed events",
+	})
+)
+
 func (h *Handler) RegisterRoutes(router *mux.Router) {
+	recordMetrics()
+
 	router.HandleFunc("/monitoring/application-metrics", h.handleApplicationMetrics).Methods("GET")
 	router.Handle("/metrics", promhttp.Handler())
 	router.HandleFunc("/monitoring/status", h.handleStatus).Methods("GET")
